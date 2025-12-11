b9484e5ae165ac75e1099d99c30cd281df08783b
Sebastian Barrera Marín <prv_sebastian.barrera@dale.com.co>
2025-12-11 12:18:07 -0500

    feat: add factory method pattern implementation and prompt for user input

---
+++ 01-Creationals/02-factory-method.ts
@@ -0,0 +1,97 @@
+/**
+ * ! Factory Method:
+ * El patrón Factory Method permite crear objetos sin especificar
+ * la clase exacta del objeto que se creará.
+ *
+ * En lugar de eso, delegamos la creación de objetos a subclases o métodos
+ * que encapsulan esta lógica.
+ *
+ * * Es útil cuando una clase no puede anticipar la clase
+ * * de objetos que debe crear.
+ *
+ * https://refactoring.guru/es/design-patterns/factory-method
+ *
+ */
+
+import { COLORS } from '../helpers/colors';
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Preparando una hamburguesa de %cpollo', COLORS.yellow);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Preparando una hamburguesa de %cres', COLORS.brown);
+  }
+}
+
+class BeanHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Preparando una hamburguesa de %cfrijol', COLORS.orange);
+  }
+}
+
+abstract class Restaurant {
+  protected abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+class BeanRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeanHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+
+  const burgerType = prompt(
+    '¿Qué tipo de hamburguesa quieres? ( chicken/beef/bean ): ',
+  );
+
+  switch (burgerType?.trim().toLowerCase()) {
+    case 'chicken':
+      restaurant = new ChickenRestaurant();
+      break;
+
+    case 'beef':
+      restaurant = new BeefRestaurant();
+      break;
+
+    case 'bean':
+      restaurant = new BeanRestaurant();
+      break;
+
+    default:
+      throw new Error('Opción no válida');
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
